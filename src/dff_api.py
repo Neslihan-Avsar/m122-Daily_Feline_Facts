e5dad56b46182607fd4b1120838d883424f22e73
Neslihan Avsar <142780276+Neslihan-Avsar@users.noreply.github.com>
2024-04-08 15:29:15 +0200

    Update dff_api.py
    Posts the actual cat fact to the discord channel!!!

--- src/dff_api.py
+++ src/dff_api.py
@@ -3,7 +3,7 @@ import json
 
 URL = 'https://catfact.ninja/fact'
 JSON = requests.get(URL, timeout=2.0)
-FACT = JSON.json().get('fact') #prints only the fact
+FACT = JSON.json().get('fact')
 
 if FACT is not None:
     print('Cat Fact: ', FACT)
@@ -11,7 +11,8 @@ else:
     print('Failed to fetch cat fact.')
 
 TOKEN = 'https://discord.com/api/webhooks/1221841988383539300/IxoAgD1alags5MQ4udTClPdm8MjmvbVbIoyNMtVViitziDVz4n0xL8rPIpLGaVMnuKJl'
-POST = '{"content": "Daily Feline Facts!", "embeds": [{"description": '+FACT+', "color": 6580991}], "attachments": []}'
+POST = {"content": "Daily Feline Facts!", "embeds": [{"description": FACT, "color": 6580991}], "attachments": []}
 HEADER = {'Content-Type': 'application/json'}
 
-print(POST) #prints the entire json body for the webhook
+RESPONSE = requests.post(TOKEN, headers=HEADER, data=json.dumps(POST))
+print('Status Code: ', RESPONSE.status_code)
