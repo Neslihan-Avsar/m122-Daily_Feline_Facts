538d6d942994da8c6a5804dc554cfb0e5f21e7ee
Neslihan Avsar <142780276+Neslihan-Avsar@users.noreply.github.com>
2024-04-08 14:43:09 +0200

    Update dff_api.py
    Can now process and seperate the keys in the json

--- src/dff_api.py
+++ src/dff_api.py
@@ -1,5 +1,11 @@
 import requests
+import json
 
-URL = "https://catfact.ninja/fact"
-JSON = requests.get(URL)
-print(JSON) #prints <Response 200>
+URL = 'https://catfact.ninja/fact'
+JSON = requests.get(URL, timeout=2.0)
+FACT = JSON.json().get('fact')
+
+if FACT is not None:
+    print('Cat Fact: ', FACT)
+else:
+    print('Failed to fetch cat fact.')
