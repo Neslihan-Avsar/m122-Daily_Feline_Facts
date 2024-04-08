9c9ba743de5b99f5f073bcb275ff06b9e21ba778
Neslihan Avsar <142780276+Neslihan-Avsar@users.noreply.github.com>
2024-04-08 15:10:18 +0200

    Update dff_api.py
    Can now format a custom json body for the post method

--- src/dff_api.py
+++ src/dff_api.py
@@ -3,9 +3,15 @@ import json
 
 URL = 'https://catfact.ninja/fact'
 JSON = requests.get(URL, timeout=2.0)
-FACT = JSON.json().get('fact')
+FACT = JSON.json().get('fact') #prints only the fact
 
 if FACT is not None:
     print('Cat Fact: ', FACT)
 else:
     print('Failed to fetch cat fact.')
+
+TOKEN = 'https://discord.com/api/webhooks/1221841988383539300/IxoAgD1alags5MQ4udTClPdm8MjmvbVbIoyNMtVViitziDVz4n0xL8rPIpLGaVMnuKJl'
+POST = '{"content": "Daily Feline Facts!", "embeds": [{"description": '+FACT+', "color": 6580991}], "attachments": []}'
+HEADER = {'Content-Type': 'application/json'}
+
+print(POST) #prints the entire json body for the webhook
