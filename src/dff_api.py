e84fbed6b898f8f91ca5350fdc11894a9c24a6ae
Neslihan Avsar <142780276+Neslihan-Avsar@users.noreply.github.com>
2024-04-15 16:09:56 +0200

    Update dff_api.py
    Fixed same image being used in message because discord caches the result per device. Now only image apis with a width and height query are allowed/possible.

--- src/dff_api.py
+++ src/dff_api.py
@@ -3,6 +3,7 @@ import json
 import configparser
 import sys
 import re
+import random
 import math
 import os
 from datetime import datetime
@@ -26,7 +27,7 @@ def errorLog(message):
     if allowErrors == 'true':
         print("ERROR: ", message)
 
-def exitSystem(exit_code):
+def exitSystemNoMessage(exit_code):
     if allowLogging == 'true':
         try:
             assert os.path.exists("dff_logging.log")
@@ -37,6 +38,22 @@ def exitSystem(exit_code):
         log = open('dff_logging.log', 'a')
         log.write("\nTIMESTAMP: " + timestamp)
         log.write("\nCODE: " + str(exit_code) + "\n")
+        log.close()
+    quit()
+# 0 = success, 1 = general failiure, 2 = config failiure, 3 = get failiure, 4 = post failiure, 5 = log failiure (theoretically)
+
+def exitSystem(exit_code, message):
+    if allowLogging == 'true':
+        try:
+            assert os.path.exists("dff_logging.log")
+        except:
+            print("Logging file is missing, please run \'dff_setup.py\' to create a new one or move the file to the same directory.")
+            quit()
+        timestamp = datetime.now().strftime("%d-%m-%Y, %H:%M:%S")
+        log = open('dff_logging.log', 'a')
+        log.write("\nTIMESTAMP: " + timestamp)
+        log.write("\nCODE: " + str(exit_code))
+        log.write("\nMESSAGE: " + message + "\n")
         log.close()
     quit()
 # 0 = success, 1 = general failiure, 2 = config failiure, 3 = get failiure, 4 = post failiure, 5 = log failiure (theoretically)
@@ -85,50 +102,58 @@ _COLOR = CONFIG.get('Discord WebHook', 'color')
 if re.search("^(http\:\/\/){1}(.+)(\.[a-zA-Z]{2,3})(\/.+)+$", URL) == True:
     warningLog("dff_config.cfg/API Website/url may not be secure, as it uses http://.")
 elif re.search("^(https?\:\/\/){1}(.+)(\.[a-zA-Z]{2,3})(\/.+)$", URL) == False:
-    errorLog("dff_config.cfg/API Website/url is not a valid URL.")
-    exitSystem(2)
+    message = "dff_config.cfg/API Website/url is not a valid URL."
+    errorLog(message)
+    exitSystem(2, message)
 else:
     debugLog("url is using https protocol and is a valid url.")
+
+if math.isnan(float(_TIMEOUT_API)):
+    message = "dff_config.cfg/API Website/timeout_api is either empty or not a number."
+    errorLog(message)
+    exitSystem(2, message)
+else:
+    TIMEOUT_API = float(_TIMEOUT_API)
 
 if _IMAGE == "":
     warningLog("dff_config/API Website/image is empty.")
 else:
-    IMAGE = _IMAGE
-
-if math.isnan(float(_TIMEOUT_API)):
-    errorLog("dff_config.cfg/API Website/timeout_api is either empty or not a number.")
-    exitSystem(2)
-else:
-    TIMEOUT_API = float(_TIMEOUT_API)
+    random =  str(random.randint(225, 255))
+    IMAGE = _IMAGE + "?width\=" + random + "&height\=" +  random
 
 JSON = requests.get(URL, timeout=TIMEOUT_API)
 FACT = JSON.json().get(JSON_PATH)
 
 if str(JSON.status_code)[0]!= '2':
-    errorLog("get request was unsuccessful.")
-    exitSystem(3)
+    message = "get request was unsuccessful."
+    errorLog(message)
+    exitSystem(3, message)
 
 if STATUSCODE_API == 'true':
     print('Status Code of the API GET Request: ', JSON.status_code)
 elif STATUSCODE_API != 'false':
-    errorLog("dff_config.cfg/API Website/display_statuscode_api is neither \'true\' or \'false\'.")
-    exitSystem(2)
+    message = "dff_config.cfg/API Website/display_statuscode_api is neither \'true\' or \'false\'."
+    errorLog(message)
+    exitSystem(2, message)
 
 # DISCORD WEBHOOK
 
 if re.search("^https://discord.com/api/webhooks/.*$", TOKEN) == False:
-    errorLog("dff_config.cfg/Discord WebHook/token is invalid. Please make sure it starts with \'https://discord.com/api/webhooks/\'.")
-    exitSystem(2)
+    message = "dff_config.cfg/Discord WebHook/token is invalid. Please make sure it starts with \'https://discord.com/api/webhooks/\'."
+    errorLog(message)
+    exitSystem(2, message)
 
 if math.isnan(float(_TIMEOUT_WEBHOOK)):
-    errorLog("dff_config.cfg/Discord WebHook/timeout_webhook is either empty or not a number.")
-    exitSystem(2)
+    message = "dff_config.cfg/Discord WebHook/timeout_webhook is either empty or not a number."
+    errorLog(message)
+    exitSystem(2, message)
 else:
     TIMEOUT_WEBHOOK = float(_TIMEOUT_WEBHOOK)
 
 if math.isnan(int(_COLOR)):
-    errorLog("dff_config.cfg/Discord WebHook/color is either empty or not a number.")
-    exitSystem(2)
+    message = "dff_config.cfg/Discord WebHook/color is either empty or not a number."
+    errorLog(message)
+    exitSystem(2, message)
 else:
     COLOR = int(_COLOR)
 
@@ -137,13 +162,15 @@ HEADER = {'Content-Type': 'application/json'}
 RESPONSE = requests.post(TOKEN, headers=HEADER, data=json.dumps(POST), timeout=TIMEOUT_WEBHOOK)
 
 if str(RESPONSE.status_code)[0]!= '2':
-    errorLog("post request was unsuccessful.")
-    exitSystem(4)
+    message = "post request was unsuccessful."
+    errorLog(message)
+    exitSystem(4, message)
 
 if STATUSCODE_WEBHOOK == 'true':
     print('Status Code of the WebHook POST Request: ', RESPONSE.status_code)
 elif STATUSCODE_WEBHOOK != 'false':
-    errorLog("dff_config.cfg/Discord WebHook/display_statuscode_webhook is neither \'true\' or \'false\'.")
-    exitSystem(2)
+    message = "dff_config.cfg/Discord WebHook/display_statuscode_webhook is neither \'true\' or \'false\'."
+    errorLog(message)
+    exitSystem(2, message)
 
-exitSystem(0)
+exitSystemNoMessage(0)
